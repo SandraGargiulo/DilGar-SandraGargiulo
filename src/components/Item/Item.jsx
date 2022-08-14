b3ee194e625b10d3d140918f55f0708c311d3ece
SandraGargiulo <sandragargiulo@gmail.com>
2022-08-14 19:17:36 -0300

    Desafio 1 con correcciones en router

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -9,17 +9,21 @@ const Item = ({ name, thumbnail, price, id, stock, brand, category }) => {
     const onAdd = (qty) => {
         alert(`Has agregado ${qty} motor/es ⚡ al carrito de compras`);
     };
+    const handleClick = (e) => {
+        e.stopPropagation()
+        console.log('hice click en item')
+    }
 
     return (
         <>
-            <Card className="product-card">
+            <Card className="product-card" onClick={handleClick}>
                 <Card.Header className="product-card__category">{category}</Card.Header>
                 <Card.Img variant="top" className="product-card__image" src={thumbnail} alt="" />
                 <Card.Body className="product-card">
                     <Card.Title className="product-card__price">${price}</Card.Title>
                     <Card.Title className="product-card__brand">{brand}</Card.Title>
                     <Card.Title className="product-card__name">{name}</Card.Title>
-                    <Link to={`${category}/${id}`} className='Option'>Ver detalle</Link>
+                    <Link to={`detail/${id}`} className='Option'>Ver detalle</Link>
                 </Card.Body>
                 <ItemCount stock={stock} onAdd={onAdd} initial={1} />
             </Card>
--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,36 +1,37 @@
 
 import { useState, useEffect } from 'react'
 import {  useParams } from 'react-router-dom'
-import ItemList from '../ItemList/ItemList.jsx' 
+import ItemList from '../ItemList/ItemList.jsx'
+import { Link } from 'react-router-dom'
 import { getProductsByCategory, getProducts } from '../../asyncMock.js'
 import './ItemListContainer.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(true)
+
     const { categoryId } = useParams()
 
     useEffect(() => {
-        setLoading(true)
-        const asyncFunction = categoryId ? getProductsByCategory : getProducts
-
-        asyncFunction(categoryId).then(response => {
-            setProducts(response)
-        }).catch(error => {
-            console.log(error)
-        }).finally(() => {
-            setLoading(false)
-        })
+        if (categoryId) {
+            getProductsByCategory(categoryId).then(response => {
+                setProducts(response)
+            })
+        } else {
+            getProducts().then(response => {
+                setProducts(response)
+            })
+        }
     }, [categoryId])
 
-    
     return (
-        <div onClick={() => console.log('click en itemlistcontainer')}>
-            <h1>{`${greeting} ${categoryId || ''}`}</h1>
-            {}
-            <ItemList products={products} />
-        </div>
+        <>
+            <div onClick={() => console.log('click en itemlistcontainer')}>
+                <h1>{`${greeting} ${categoryId || ''}`}</h1>
+                {}
+                <ItemList products={products} />
+            </div>
+        </>
     )
 }
 
